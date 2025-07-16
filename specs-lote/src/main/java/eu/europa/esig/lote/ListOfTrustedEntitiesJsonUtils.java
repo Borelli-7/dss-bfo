108d2d00af982eab6590032515ae0f2a7ac3ac42
pvandenbroucke <pierrick.vandenbroucke@nowina.lu>
2025-07-16 17:43:43 +0200

    Quick test of LoTE

---
+++ specs-lote/src/main/java/eu/europa/esig/lote/ListOfTrustedEntitiesJsonUtils.java
@@ -0,0 +1,75 @@
+/**
+ * DSS - Digital Signature Services
+ * Copyright (C) 2015 European Commission, provided under the CEF programme
+ * <p>
+ * This file is part of the "DSS - Digital Signature Services" project.
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package eu.europa.esig.lote;
+
+import eu.europa.esig.json.JSONSchemaAbstractUtils;
+
+import java.net.URI;
+import java.util.Map;
+
+/**
+ * This class contains utils for parsing and validation of the list of Trusted Entities to
+ * the ETSI TS 119 602 JSON schema
+ *
+ */
+public final class ListOfTrustedEntitiesJsonUtils extends JSONSchemaAbstractUtils {
+
+    /** LoTE schema URI */
+    private static final String SCHEMA_URI =  "http://uri.etsi.org/19602/ExplicitScheme/JSONSchema";
+
+    /** LoTE schema's location */
+    private static final String SCHEMA_LOCATION = "/schema/19602_json_schema.json";
+
+    /** Singleton instance */
+    private static ListOfTrustedEntitiesJsonUtils singleton;
+
+    /**
+     * Empty constructor
+     */
+    private ListOfTrustedEntitiesJsonUtils() {
+        // empty
+    }
+
+    /**
+     * Returns instance of {@code ListOfTrustedEntitiesJsonUtils}
+     *
+     * @return {@link ListOfTrustedEntitiesJsonUtils}
+     */
+    public static ListOfTrustedEntitiesJsonUtils getInstance() {
+        if (singleton == null) {
+            singleton = new ListOfTrustedEntitiesJsonUtils();
+        }
+        return singleton;
+    }
+
+    @Override
+    public String getSchemaURI() {
+        return SCHEMA_URI;
+    }
+
+    @Override
+    public Map<URI, String> getSchemaDefinitions() {
+        Map<URI, String> definitions = getJSONSchemaDefinitions();
+        definitions.put(URI.create(SCHEMA_URI), SCHEMA_LOCATION);
+        return definitions;
+    }
+
+}

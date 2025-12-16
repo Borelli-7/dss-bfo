a260cd1a35b65ec88bab1beb7facda0130a28ead
Aleksandr Beliakov <aleksandr.beliakov@nowina.lu>
2025-12-16 11:50:55 +0100

    DSS-3783 : fix validation for third country Trust Service with defined qualifiers, when no MRA mapping is provided

---
+++ dss-tsl-validation/src/test/java/eu/europa/esig/dss/tsl/job/mra/MRANoQualifiersMappingTest.java
@@ -0,0 +1,36 @@
+package eu.europa.esig.dss.tsl.job.mra;
+
+import eu.europa.esig.dss.enumerations.Indication;
+import eu.europa.esig.dss.enumerations.SignatureQualification;
+
+import java.util.Collections;
+import java.util.Map;
+
+class MRANoQualifiersMappingTest extends AbstractMRALOTLTest {
+
+    @Override
+    protected Map<String, String> getQualifierEquivalenceMap() {
+        return Collections.emptyMap();
+    }
+
+    @Override
+    protected Indication getFinalIndication() {
+        return Indication.TOTAL_PASSED;
+    }
+
+    @Override
+    protected SignatureQualification getFinalSignatureQualification() {
+        return SignatureQualification.QESIG;
+    }
+
+    @Override
+    protected boolean isEnactedMRA() {
+        return true;
+    }
+
+    @Override
+    protected String getMRAEnactedTrustServiceLegalIdentifier() {
+        return "QCForESig";
+    }
+
+}
